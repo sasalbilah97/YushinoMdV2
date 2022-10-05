c3cc0bd39a3645dd71530f9fcff17cc94ff2a365
Sasal66 <107730821+Sasal66@users.noreply.github.com>
2022-10-06 00:12:23 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -18,7 +18,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 ${description}
 ◈ ━┉┈┄┈┈ ►
 
-script : https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg
+gcbot  : https://chat.whatsapp.com/JVFN0SYnXLg23qjvickkdM
 `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
